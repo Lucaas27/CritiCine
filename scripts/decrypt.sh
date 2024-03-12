0152a593c8fea67d1d32b2d2ea50c6a651104897
Lucas Gomes <60785299+Lucaas27@users.noreply.github.com>
2024-03-12 17:15:06 +0000

    Set up CI/CD Pipeline (#2)
    * Set up docker
    
    * Updated docker compose files
    
    * Initial pipeline set up
    
    * Update pipeline to work with application built in dockerfile
    
    * Update pipeline to try and fix docker login issues
    
    * Encrypt secrets
    
    * Update dockerfile
    
    * Update dockerfile to fix sintax
    
    * Update dockerfile to fix sintax

---
+++ scripts/decrypt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Extract the key ID from the output of 'gpg --list-secret-keys'
+key_id=$(gpg --list-secret-keys | grep -oP '^\s*\w{40}')
+
+# Check if the key ID is not empty
+if [ -n "$key_id" ]; then
+    # Loop through .env* files and encrypt them using the extracted key ID
+    for file in .env*; do
+        if [ -f "$file" ] && [[ ! "$file" =~ \.enc$ ]]; then
+            sops --decrypt \
+            --pgp "$key_id" \
+            "$file" > "${file%.enc}"
+        fi
+    done
+else
+    echo "Error: No secret key found."
+fi
