0fb5d1a19ab7c6975e97fb1ffe2d6d3c2b501232
Lucas Gomes <lucasggomees@gmail.com>
2024-03-12 23:16:21 +0000

    Set up initial dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3000,
     strictPort: true,
@@ -14,4 +20,4 @@ export default defineConfig({
     emptyOutDir: true,
     // rollupOptions: {},
   },
-});
+})
